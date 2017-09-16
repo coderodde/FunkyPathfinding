c185f0d23711c5317e1fd64ea76d11516402674f
coderodde <rodion.efremov@cs.helsinki.fi>
2017-09-16 15:52:47 +0300

    ..

--- src/net/coderodde/funky/pathfinding/NewBidirectionalAStarPathfinder.java
+++ src/net/coderodde/funky/pathfinding/NewBidirectionalAStarPathfinder.java
@@ -88,154 +88,152 @@ extends AbstractPathfinder {
                 panel.repaint();
             }
             
-            if (openForward.size() < openBackward.size()) {
-                Point currentPoint = openForward.extractMinimum();
-                closed.add(currentPoint);
-                panel.markAsClosed(currentPoint);
-                this.closedNodeCount++;
-                
-                double distance1 = distancesForward.get(currentPoint) +
-                                   currentPoint.distance(targetPoint);
-                
-                double distance2 = distancesForward.get(currentPoint) +
-                                   fBackward -
-                                   currentPoint.distance(sourcePoint);
-                
-                if (Math.max(distance1, distance2) >= bestPathLength) {
+            Point currentPoint = openForward.extractMinimum();
+            closed.add(currentPoint);
+            panel.markAsClosed(currentPoint);
+            this.closedNodeCount++;
+
+            double distance1 = distancesForward.get(currentPoint) +
+                               currentPoint.distance(targetPoint);
+
+            double distance2 = distancesForward.get(currentPoint) +
+                               fBackward -
+                               currentPoint.distance(sourcePoint);
+
+            if (Math.max(distance1, distance2) >= bestPathLength) {
+                continue;
+            }
+
+            for (Point childPoint : panel.expand(currentPoint)) {
+                if (closed.contains(childPoint)) {
                     continue;
                 }
-                
-                for (Point childPoint : panel.expand(currentPoint)) {
-                    if (closed.contains(childPoint)) {
-                        continue;
-                    }
-                    
-                    double tentativeDistance = 
-                            distancesForward.get(currentPoint) +
-                            currentPoint.distance(childPoint);
-                    
-                    if (!distancesForward.containsKey(childPoint)) {
-                        distancesForward.put(childPoint, tentativeDistance);
-                        parentsForward.put(childPoint, currentPoint);
-                        
-                        openForward.add(childPoint,
-                                        tentativeDistance +
-                                        childPoint.distance(targetPoint));
-                        
-                        panel.markAsFrontier(childPoint);
-                        
-                        if (distancesBackward.containsKey(childPoint)) {
-                            double pathLength = 
+
+                double tentativeDistance = 
+                        distancesForward.get(currentPoint) +
+                        currentPoint.distance(childPoint);
+
+                if (!distancesForward.containsKey(childPoint)) {
+                    distancesForward.put(childPoint, tentativeDistance);
+                    parentsForward.put(childPoint, currentPoint);
+
+                    openForward.add(childPoint,
                                     tentativeDistance +
-                                    distancesBackward.get(childPoint);
-                            
-                            if (bestPathLength > pathLength) {
-                                bestPathLength = pathLength;
-                                meetingPoint = childPoint;
-                            }
-                        }
-                    } else if (distancesForward.get(childPoint) 
-                            > tentativeDistance) {
-                        distancesForward.put(childPoint, tentativeDistance);
-                        parentsForward.put(childPoint, currentPoint);
-                        
-                        openForward.decreasePriority(
-                                childPoint,
+                                    childPoint.distance(targetPoint));
+
+                    panel.markAsFrontier(childPoint);
+
+                    if (distancesBackward.containsKey(childPoint)) {
+                        double pathLength = 
                                 tentativeDistance +
-                                        childPoint.distance(targetPoint));
-                        
-                        if (distancesBackward.containsKey(childPoint)) {
-                            double pathLength = 
-                                    tentativeDistance +
-                                    distancesBackward.get(childPoint);
-                            
-                            if (bestPathLength > pathLength) {
-                                bestPathLength = pathLength;
-                                meetingPoint = childPoint;
-                            }
+                                distancesBackward.get(childPoint);
+
+                        if (bestPathLength > pathLength) {
+                            bestPathLength = pathLength;
+                            meetingPoint = childPoint;
+                        }
+                    }
+                } else if (distancesForward.get(childPoint) 
+                        > tentativeDistance) {
+                    distancesForward.put(childPoint, tentativeDistance);
+                    parentsForward.put(childPoint, currentPoint);
+
+                    openForward.decreasePriority(
+                            childPoint,
+                            tentativeDistance +
+                                    childPoint.distance(targetPoint));
+
+                    if (distancesBackward.containsKey(childPoint)) {
+                        double pathLength = 
+                                tentativeDistance +
+                                distancesBackward.get(childPoint);
+
+                        if (bestPathLength > pathLength) {
+                            bestPathLength = pathLength;
+                            meetingPoint = childPoint;
                         }
                     }
                 }
+            }
+
+            if (openForward.size() > 0) {
+                Point point = openForward.top();
+                fForward = distancesForward.get(point) + 
+                        point.distance(targetPoint);
+            }
                 
-                if (openForward.size() > 0) {
-                    Point point = openForward.top();
-                    fForward = distancesForward.get(point) + 
-                            point.distance(targetPoint);
-                }
-            } else {
-                Point currentPoint = openBackward.extractMinimum();
-                closed.add(currentPoint);
-                panel.markAsClosed(currentPoint);
-                this.closedNodeCount++;
-                
-                double distance1 = distancesBackward.get(currentPoint) +
-                                   currentPoint.distance(sourcePoint);
-                
-                double distance2 = distancesBackward.get(currentPoint) +
-                                   fForward -
-                                   currentPoint.distance(targetPoint);
-                
-                if (Math.max(distance1, distance2) >= bestPathLength) {
+            currentPoint = openBackward.extractMinimum();
+            closed.add(currentPoint);
+            panel.markAsClosed(currentPoint);
+            this.closedNodeCount++;
+
+            distance1 = distancesBackward.get(currentPoint) +
+                               currentPoint.distance(sourcePoint);
+
+            distance2 = distancesBackward.get(currentPoint) +
+                               fForward -
+                               currentPoint.distance(targetPoint);
+
+            if (Math.max(distance1, distance2) >= bestPathLength) {
+                continue;
+            }
+
+            for (Point parentPoint : panel.expand(currentPoint)) {
+                if (closed.contains(parentPoint)) {
                     continue;
                 }
-                
-                for (Point parentPoint : panel.expand(currentPoint)) {
-                    if (closed.contains(parentPoint)) {
-                        continue;
-                    }
-                    
-                    double tentativeDistance = 
-                            distancesBackward.get(currentPoint) +
-                            parentPoint.distance(currentPoint);
-                    
-                    if (!distancesBackward.containsKey(parentPoint)) {
-                        distancesBackward.put(parentPoint, tentativeDistance);
-                        parentsBackward.put(parentPoint, currentPoint);
-                        
-                        openBackward.add(parentPoint,
-                                         tentativeDistance + 
-                                            parentPoint.distance(sourcePoint));
-                        
-                        panel.markAsFrontier(parentPoint);
-                        
-                        if (distancesForward.containsKey(parentPoint)) {
-                            double pathLength = 
-                                    tentativeDistance + 
-                                    distancesForward.get(parentPoint);
-                            
-                            if (bestPathLength > pathLength) {
-                                bestPathLength = pathLength;
-                                meetingPoint = parentPoint;
-                            }
-                        }
-                    } else if (distancesBackward.get(parentPoint) 
-                            > tentativeDistance) {
-                        distancesBackward.put(parentPoint, tentativeDistance);
-                        parentsBackward.put(parentPoint, currentPoint);
-                        
-                        openBackward.decreasePriority(
-                                parentPoint,
-                                tentativeDistance +
+
+                double tentativeDistance = 
+                        distancesBackward.get(currentPoint) +
+                        parentPoint.distance(currentPoint);
+
+                if (!distancesBackward.containsKey(parentPoint)) {
+                    distancesBackward.put(parentPoint, tentativeDistance);
+                    parentsBackward.put(parentPoint, currentPoint);
+
+                    openBackward.add(parentPoint,
+                                     tentativeDistance + 
                                         parentPoint.distance(sourcePoint));
-                        
-                        if (distancesForward.containsKey(parentPoint)) {
-                            double pathLength = 
-                                    tentativeDistance + 
-                                    distancesForward.get(parentPoint);
-                            
-                            if (bestPathLength > pathLength) {
-                                bestPathLength = pathLength;
-                                meetingPoint = parentPoint;
-                            }
+
+                    panel.markAsFrontier(parentPoint);
+
+                    if (distancesForward.containsKey(parentPoint)) {
+                        double pathLength = 
+                                tentativeDistance + 
+                                distancesForward.get(parentPoint);
+
+                        if (bestPathLength > pathLength) {
+                            bestPathLength = pathLength;
+                            meetingPoint = parentPoint;
+                        }
+                    }
+                } else if (distancesBackward.get(parentPoint) 
+                        > tentativeDistance) {
+                    distancesBackward.put(parentPoint, tentativeDistance);
+                    parentsBackward.put(parentPoint, currentPoint);
+
+                    openBackward.decreasePriority(
+                            parentPoint,
+                            tentativeDistance +
+                                    parentPoint.distance(sourcePoint));
+
+                    if (distancesForward.containsKey(parentPoint)) {
+                        double pathLength = 
+                                tentativeDistance + 
+                                distancesForward.get(parentPoint);
+
+                        if (bestPathLength > pathLength) {
+                            bestPathLength = pathLength;
+                            meetingPoint = parentPoint;
                         }
                     }
                 }
-                
-                if (openBackward.size() > 0) {
-                    Point point = openBackward.top();
-                    fBackward = distancesBackward.get(point) + 
-                                point.distance(sourcePoint);
-                }
+            }
+
+            if (openBackward.size() > 0) {
+                Point point = openBackward.top();
+                fBackward = distancesBackward.get(point) + 
+                            point.distance(sourcePoint);
             }
         }
         
